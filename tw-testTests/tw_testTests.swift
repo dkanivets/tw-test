ab2e47d588b032c1f9541c99c76ea26e5b00d881
Dmitry Kanivets <dkanivetsua@gmail.com>
2018-04-06 21:44:33 +0300

    Initial Commit

---
+++ tw-testTests/tw_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  tw_testTests.swift
+//  tw-testTests
+//
+//  Created by Dmitry Kanivets on 06.04.18.
+//  Copyright © 2018 Dmitry Kanivets. All rights reserved.
+//
+
+import XCTest
+@testable import tw_test
+
+class tw_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
